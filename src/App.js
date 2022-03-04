6032e9ef4a0867c21d6af4f7683776fbb08fa5d7
shecodesjele <jelena.todorovic11@gmail.com>
2022-03-04 12:49:30 +0100

    Initial React setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header>Dictionary</header>
+        <footer>Coded by Jelena</footer>
+      </div>
+    </div>
+  );
+}
